aa5709cdf8685391ed2cb95d30e89c1f6f8dfa5b
lheazel <lheazel@gmail.com>
2018-11-15 09:13:10 -0500

    First commit. Nginx and php-fpm working.

---
+++ code/index.php
@@ -0,0 +1,14 @@
+<html>
+  <header>
+    <title>This is title</title>
+  </header>
+  <body>
+    <p>Hello World HTML!</p>
+    <p>
+      <?php
+      echo "Hello World PHP!";
+      phpinfo();
+      ?>
+    </p>
+  </body>
+</html>
